919ebc9bee3837d32cfc94c54c4c03f7174cf62e
Santiago Sala <pipo0716@gmail.com>
2025-05-31 17:48:39 +0300

    Some changes to the input. Now selecting items is done after release of mouse button

--- Assets/Project_MatchFactory/Scripts/InputManager.cs
+++ Assets/Project_MatchFactory/Scripts/InputManager.cs
@@ -5,6 +5,9 @@ public class InputManager : MonoBehaviour
 {
     public static Action<Item> itemClicked;
 
+    [Header(" Settings ")]
+    private Item m_currentItem;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,29 +17,44 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            HandleMouseDown();
+            HandleDrag();
         }
-        
+        else if (Input.GetMouseButtonUp(0))
+        {
+            HandleMouseUp();
+        }
     }
-
-    private void HandleMouseDown()
+    private void HandleDrag()
     {
         Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo, 100);
 
         if (hitInfo.collider == null)
         {
+            m_currentItem = null;
             return;
         }
 
         if(!hitInfo.collider.TryGetComponent(out Item item))
         {
+            m_currentItem = null;
             return;
         }
 
         Debug.Log($"Hit: {hitInfo.collider.name} at {hitInfo.point}");
 
-        itemClicked?.Invoke(item);
+        m_currentItem = item;
+        
+    }
+    private void HandleMouseUp()
+    {
+        if(m_currentItem == null)
+        {
+            return;
+        }
+
+        itemClicked?.Invoke(m_currentItem);
+        m_currentItem = null;
     }
 }
